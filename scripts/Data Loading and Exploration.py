5cb43dfb6763b0c9ab896ca2cfca33d7d0807e17
STEFANOS METZIDAKIS <stefanosmetzidakis@yahoo.gr>
2024-04-15 12:20:33 +0300

    #Data Loading and Exploration
    ADDED Data Loading and Exploration

---
+++ scripts/Data Loading and Exploration.py
@@ -0,0 +1,20 @@
+
+#Data Loading and Exploration
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Load data from CSV file
+data = pd.read_csv("Quality Of Service 5G Dataset.csv")
+
+# Explore data
+data.head()
+data.info()
+data.describe()
+
+# Check for missing values
+data.isnull().sum()
